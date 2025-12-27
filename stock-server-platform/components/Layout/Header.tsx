0ba0cb7d89ca796b981a6f4dba349ba7e62fd2c8
kasra <kasramajdy81@gmail.com>
2025-12-28 01:32:49 +0330

    perf: optimize images for better LCP performance

--- stock-server-platform/components/Layout/Header.tsx
+++ stock-server-platform/components/Layout/Header.tsx
@@ -35,6 +35,8 @@ export default function Header({
                   fill
                   className="object-contain"
                   priority
+                  quality={85}
+                  loading="eager"
                 />
               </div>
             </Link>
@@ -85,6 +87,8 @@ export default function Header({
                 fill
                 className="object-contain"
                 priority
+                quality={85}
+                loading="eager"
               />
             </Link>
             <div className="flex-1 flex justify-center lg:px-6 xl:px-8">
